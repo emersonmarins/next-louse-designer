35b9d5ee1518cadaa8ba034f01255d30294de408
Emerson <emerson@Debian.emersonmarins>
2024-03-05 23:16:17 -0300

    modified style card

--- my-portfolio-designer/src/app/home/components/card/card.tsx
+++ my-portfolio-designer/src/app/home/components/card/card.tsx
@@ -4,7 +4,7 @@ import Text from "../elements/text"
 
 export default function Card(options: any) {
   return (
-    <div className="shadow-2xl relative mb-24 h-[500px] text-center max-sm:w-[90%] max-sm:rounded-3xl m-auto bg-[linear-gradient(to_right,_#373737,_#212121)]">
+    <div className="shadow-2xl relative mb-24 h-[500px] text-center max-w-[700px] max-sm:w-[90%] max-sm:rounded-3xl m-auto bg-[linear-gradient(to_right,_#373737,_#212121)]">
       <div className="h-1"></div>
       <div className="flex flex-col gap-2 py-7 mt-4 bg-zinc-50">
         <Text options={{ tag: 'span', text: 'Design', fontSize: '1.7', color: "#272727", textTransform: 'uppercase', fontWeight: '600' }} />
